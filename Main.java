71532b0f9b034572e5ed74b53509c650b824aafd
Rafiul-Hoque <115275399+Rafiul-Hoque@users.noreply.github.com>
2022-11-02 20:34:19 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package myfirstproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("i love pizza");
+		System.out.println("they are delicious");
+	
+	}
+
+}
